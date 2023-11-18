cfb28b03b75656f9095d757936ad1ae9d6c0c477
Marco <marco.isfc@gmail.com>
2023-11-18 18:56:44 +0100

    Init

--- src/JsonErrorResponse.php
+++ src/JsonErrorResponse.php
@@ -1,12 +1,30 @@
 <?php
 
 namespace Siestacat\SymfonyJsonErrorResponse;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Psr\Log\LoggerInterface;
 
 /**
  * @package Siestacat\SymfonyJsonErrorResponse
  */
-class JsonErrorResponse
+abstract class JsonErrorResponse extends AbstractController
 {
+    protected function json_error(\Exception $e, ?LoggerInterface $logger = null, array $json):JsonResponse
+    {
+        if($logger !== null) $logger->error($e->getMessage());
 
-    
+        $error_message = !($this->getParameter('kernel.environment') === 'prod') ? $e->getMessage() . "\n" . $e->getTraceAsString() : null;
+
+        return $this->json_error_message($error_message, $json);
+    }
+
+    protected function json_error_message(?string $error_message, array $json = []):JsonResponse
+    {
+        $json['error'] = $error_message;
+
+        $json['success'] = false;
+
+        return $this->json($json)->setStatusCode(500);
+    }
 }
--- tests/JsonErrorResponseTest.php
+++ tests/JsonErrorResponseTest.php
@@ -6,5 +6,8 @@ use PHPUnit\Framework\TestCase;
 
 class JsonErrorResponseTest extends TestCase
 {
-    
+    public function test()
+    {
+        $this->assertEquals(true, true); //Pending test
+    }
 }
